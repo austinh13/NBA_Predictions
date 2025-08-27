092dbbfd3ec92197e327bc42ebf83b168ea395a2
austinh13 <austinha236@gmail.com>
2025-08-26 22:42:22 -0500

    web fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import VisualData from './pages/VisualData'
 import AllNBA from './pages/AllNBA'
 import "./styles/chartStyle.css"
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from "@vercel/analytics";
+
+inject();
 
 function App() {
 
