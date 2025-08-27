a4e290f08d280b521312d5a9c5022c23d920b0af
austinh13 <austinha236@gmail.com>
2025-08-26 22:39:10 -0500

    removed router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import VisualData from './pages/VisualData'
 import AllNBA from './pages/AllNBA'
 import "./styles/chartStyle.css"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/next"
 
 function App() {
